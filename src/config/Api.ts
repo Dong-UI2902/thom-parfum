f9d49dbb6cc2878a9ab52b2bc5eff2926c8b59e8
Dong-UI2902 <dongnv2902@gmail.com>
2025-04-04 16:29:09 +0700

    change url api

--- src/config/Api.ts
+++ src/config/Api.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const PATH =  process.env.NODE_ENV === "development" ? "http://localhost:3000/" : "https://thomparfum.vn";
+const PATH =  process.env.NODE_ENV === "development" ? "http://localhost:3000/" : "https://thomparfum.com";
 //TODO: fix url
 export const axiosClient = axios.create({
   // withCredentials: true,
   baseURL: PATH,
   // headers: {
-  //   "Access-Control-Allow-Origin": "https://thomparfum.vn, https://www.thomparfum.vn",
+  //   "Access-Control-Allow-Origin": "https://thomparfum.vn, https://www.thomparfum.com",
   // }
 });
 
